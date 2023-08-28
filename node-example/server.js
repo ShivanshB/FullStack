cf049d5ddb9d835bc703ba09868697000be5c118
Shivansh Baveja <shivansh.baveja@berkeley.edu>
2023-08-28 13:52:14 -0700

    local host retrieved html file

--- node-example/server.js
+++ node-example/server.js
@@ -4,9 +4,11 @@ const port = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    // res.send('Hello World');  Sending Text Directly
+    // res.send('<h1>This should be rendered in HTML</h1>'); Sending HTML directly
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at https://localhost:${port}');
+    console.log(`Example app listening at https://localhost:${port}`);
 });
