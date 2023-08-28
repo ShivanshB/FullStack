a0303110e550e7f8d795da1c80d2c54dbae85ef5
Shivansh Baveja <shivansh.baveja@berkeley.edu>
2023-08-28 14:12:39 -0700

    implemented basic data persistence

--- node-example/server.js
+++ node-example/server.js
@@ -5,10 +5,18 @@ let totalViews = 0
 
 const app = express();
 
+if (fs.existsSync('views.txt')) {
+    totalViews = parseInt(fs.readFileSync('views.txt', 'utf8'));
+}
+    
+
 app.get('/', (req, res) => {
     // res.send('Hello World');  Sending Text Directly
     // res.send('<h1>This should be rendered in HTML</h1>'); Sending HTML directly
     totalViews++;
+
+    fs.writeFileSync('views.txt', String(totalViews));
+
     let html = fs.readFileSync(__dirname + '/index.html', 'utf8')
     html = html.replace('{{views}}', totalViews)
     res.send(html);
