e5c8cacdaf4d1ace6bee165ff9c94c7509267031
Shivansh Baveja <shivansh.baveja@berkeley.edu>
2023-08-28 14:35:08 -0700

    added server-side cookies

--- node-example/server.js
+++ node-example/server.js
@@ -5,20 +5,33 @@ let totalViews = 0
 
 const app = express();
 
-if (fs.existsSync('views.txt')) {
-    totalViews = parseInt(fs.readFileSync('views.txt', 'utf8'));
-}
+// if (fs.existsSync('views.txt')) {
+//     totalViews = parseInt(fs.readFileSync('views.txt', 'utf8'));
+// }
     
+function getCookie(cookieString, name) {
+    if (cookieString) {
+        let cookie = cookieString.split(';').find(cookie => cookie.trim().startsWith(`${name}=`));
+        return cookie.split('=')[1];
+    }
+    else {
+        return 0;
+    }
+}
 
 app.get('/', (req, res) => {
     // res.send('Hello World');  Sending Text Directly
     // res.send('<h1>This should be rendered in HTML</h1>'); Sending HTML directly
+    let totalViews = getCookie(req.headers.cookie, 'views');
+
     totalViews++;
 
-    fs.writeFileSync('views.txt', String(totalViews));
+    // fs.writeFileSync('views.txt', String(totalViews));
 
-    let html = fs.readFileSync(__dirname + '/index.html', 'utf8')
-    html = html.replace('{{views}}', totalViews)
+    let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+    html = html.replace('{{views}}', totalViews);
+
+    res.setHeader('Set-Cookie', `views=${totalViews}`);
     res.send(html);
 });
 
