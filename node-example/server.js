e936f3de52aac96ea817cbbea1960f7cf50ebac4
Shivansh Baveja <shivansh.baveja@berkeley.edu>
2023-08-28 14:06:05 -0700

    enabled server side rendering

--- node-example/server.js
+++ node-example/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 const port = 3000;
+let totalViews = 0
 
 const app = express();
 
 app.get('/', (req, res) => {
     // res.send('Hello World');  Sending Text Directly
     // res.send('<h1>This should be rendered in HTML</h1>'); Sending HTML directly
-    res.sendFile(__dirname + '/index.html');
+    totalViews++;
+    let html = fs.readFileSync(__dirname + '/index.html', 'utf8')
+    html = html.replace('{{views}}', totalViews)
+    res.send(html);
 });
 
-
-app.get('/styles.css', (req, res) => {
-    res.sendFile(__dirname + '/styles.css');
-});
 
 
 app.listen(port, () => {
